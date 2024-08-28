ac0dfb447eb095f35bf8c968b39ef2205819e30b
Robert Wiebe <robert.wiebe@luminis.eu>
2024-08-28 13:20:05 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className='w-full h-full flex items-center justify-center'>
+      <h1>Hello world!</h1>
+    </div>
+  )
+}
+
+export default App
